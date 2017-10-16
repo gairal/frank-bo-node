ae62a738ffa3fbe0a93726b9ba45c8873e5d85e5
frank.gairal <frank.gairal@fullsix.com>
2017-10-16 17:56:57 -0400

    feat(datastore): transforming remaining methods

--- src/services/database.js
+++ src/services/database.js
@@ -7,97 +7,103 @@ class Db {
     this.dConn = dConn;
   }
 
-  async getAll(table, withImage = false) {
+  async getAll(kind, withImage = false) {
     let result = [];
     try {
-      let q;
       if (!withImage) {
-        q = this.dConn.createQuery([table]);
+        const q = this.dConn.createQuery(kind);
+        result = await q.run();
       } else {
-        // cursor = await r.table(table).eqJoin('image_id', r.table('image'))
-        //   .map(doc => doc.merge(() => ({ right: { image: doc('right')('name') } })))
-        //   .without({
-        //     left: {
-        //       image_id: true,
-        //     },
-        //     right: {
-        //       id: true,
-        //       name: true,
-        //     },
-        //   })
-        //   .zip()
-        //   .run(this.rdbConn);
-        q = this.dConn.createQuery([table]);
+        const qKind = this.dConn.createQuery(kind);
+        const qImgs = this.dConn.createQuery('image');
+        result = await qKind.run();
+        const imgRes = await qImgs.run();
+
+        const imgs = imgRes[0];
+        const kinds = result[0];
+
+        // Order images in Object for fast access
+        const imgsObj = {};
+        let currImgId;
+        for (let i = 0, l = imgs.length; i < l; i += 1) {
+          currImgId = imgs[i][Datastore.KEY].id;
+          imgsObj[currImgId] = imgs[i];
+        }
+
+        for (let i = 0, l = kinds.length; i < l; i += 1) {
+          kinds[i].image = imgsObj[kinds[i].image.id];
+        }
+      }
+    } catch (err) {
+      if (err) throw err;
+    }
+
+    return result[0];
+  }
+
+  async getByCategory(kind, category) {
+    let result;
+    try {
+      const q = this.dConn.createQuery(kind)
+        .filter('category', '=', this.dConn.key(['category', +category]));
+      result = await q.run();
+    } catch (err) {
+      if (err) throw err;
+    }
+
+    return result;
+  }
+
+  async getAllByCategory(kind) {
+    const result = [];
+    try {
+      const qKind = this.dConn.createQuery(kind);
+      const qCats = this.dConn.createQuery('category');
+      const tableRes = await qKind.run();
+      const catRes = await qCats.run();
+
+      const kinds = tableRes[0];
+      const cats = catRes[0];
+
+      // Order categories in Object for fast access
+      const catsObj = {};
+      let currCatId;
+      for (let i = 0, l = cats.length; i < l; i += 1) {
+        currCatId = cats[i][Datastore.KEY].id;
+        catsObj[currCatId] = cats[i];
       }
 
-      result = await this.dConn.runQuery(q);
+      let kindCatId;
+      let currCat;
+      for (let i = 0, l = kinds.length; i < l; i += 1) {
+        kindCatId = kinds[i].category.id;
+        currCat = catsObj[kindCatId];
+        if (typeof catsObj[kindCatId].index === 'undefined') {
+          const newLength = result.push(Object.assign({ skills: [] }, currCat));
+          currCat.index = newLength - 1;
+        }
+
+        delete kinds[i].category;
+        result[currCat.index].skills.push(kinds[i]);
+      }
     } catch (err) {
       if (err) throw err;
     }
 
-    return result[0];
+    return result;
   }
 
-  // async getByCategory(table, category) {
-  //   let result;
-  //   try {
-  //     const cursor = await r.table(table).filter({ category_id: category }).run(this.rdbConn);
-  //     result = await cursor.toArray();
-
-  //     return result;
-  //   } catch (err) {
-  //     if (err) throw err;
-  //   }
-
-  //   return result;
-  // }
-
-  async getAllByCategory(table) {
-    let result = [];
-    try {
-      // const cursor = await r.table('category').merge((obj) => {
-      //   const ret = {};
-      //   ret[`${table}s`] = r.table(table).getAll(obj('id'),
-      //     { index: 'category_id' }).coerceTo('array');
-      //   return ret;
-      // }).run(this.rdbConn);
-      // result = await cursor.toArray();
-
-      const q = this.dConn.createQuery(table);
-      result = await this.dConn.runQuery(q);
-    } catch (err) {
-      if (err) throw err;
-    }
-
-    console.log(result[0][0][Datastore.KEY].id);
-
-    return result[0];
-  }
-
-  async getById(table, id, withImage = false) {
+  async getById(kind, id, withImage = false) {
     let result = [];
 
     try {
-      if (!withImage) {
-        const key = this.dConn.key([table, +id]);
-        result = await this.dConn.get(key);
-      } else {
-        // const cursor = await r.table(table).filter({ id }).eqJoin('image_id', r.table('image'))
-        //   .map(doc => doc.merge(() => ({ right: { image: doc('right')('name') } })))
-        //   .without({
-        //     left: {
-        //       image_id: true,
-        //     },
-        //     right: {
-        //       id: true,
-        //       name: true,
-        //     },
-        //   })
-        //   .zip()
-        //   .run(this.rdbConn);
-        // result = await cursor.next();
-        const key = this.dConn.key([table, +id]);
-        result = await this.dConn.get(key);
+      const key = this.dConn.key([kind, +id]);
+      result = await this.dConn.get(key);
+
+      if (withImage) {
+        const imgKey = this.dConn.key(['image', +result[0].image.id]);
+        const img = await this.dConn.get(imgKey);
+        result[0].image = img[0];
       }
     } catch (err) {
       if (err) throw err;
