a32603ca7b39be5e15b4450e3db91835102e34e2
frank.gairal <frank@gairal.com>
2022-01-11 18:03:15 +0100

    fix: education done

--- scripts/exportFirestore.ts
+++ scripts/exportFirestore.ts
@@ -1,13 +1,45 @@
+import { Timestamp } from "@google-cloud/firestore";
+
 import { firestore } from "../src/lib/db";
+import { categoryFixture } from "../test/fixtures/category";
 import { educationFixture } from "../test/fixtures/education";
 import { imageFixture } from "../test/fixtures/image";
+
+export const insertCategories = async () => {
+  const cats = categoryFixture();
+
+  await Promise.all(
+    cats.map(({ name, order }) =>
+      firestore
+        .collection("category")
+        .doc(name.toLowerCase().replace(" ", "-"))
+        .set({ label: name, order })
+    )
+  );
+};
 
 export const insertEducations = async () => {
   const edus = educationFixture();
 
   await Promise.all(
-    edus.map((edu) =>
-      firestore.collection("education").doc(edu.name.toLowerCase()).set(edu)
+    edus.map(
+      ({
+        dateIn,
+        dateOut,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        id,
+        image,
+        ...rest
+      }) =>
+        firestore
+          .collection("education")
+          .doc(image.id)
+          .set({
+            ...rest,
+            dateIn: new Timestamp(dateIn.seconds, dateIn.nanoseconds),
+            dateOut: new Timestamp(dateOut.seconds, dateOut.nanoseconds),
+            image: firestore.doc(image.path.join("/")),
+          })
     )
   );
 };
@@ -16,17 +48,14 @@ export const insertImages = async () => {
   const imgs = imageFixture();
 
   await Promise.all(
-    imgs.map((img) =>
-      firestore
-        .collection("image")
-        .doc(img.name.split(".")[0])
-        .set({ file: img.name })
+    imgs.map(({ file }) =>
+      firestore.collection("image").doc(file.split(".")[0]).set({ file })
     )
   );
 };
 
 const run = async () => {
-  await insertImages();
+  await insertEducations();
 };
 
 run()
