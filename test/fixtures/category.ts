b61c38149a4ad5e3fbc5148e8518abfb1511c82a
frank.gairal <frank@gairal.com>
2022-01-12 21:53:54 +0100

    fix: tests

--- test/fixtures/category.ts
+++ test/fixtures/category.ts
@@ -1,69 +1,17 @@
 import { FirestoreCategory } from "../../src/types/category";
 
 export const categoryFixture = (): FirestoreCategory[] => [
-  {
-    id: "database",
-    label: "Database",
-    order: 5,
-  },
-  {
-    id: "miscellaneous",
-    label: "Miscellaneous",
-    order: 11,
-  },
-  {
-    id: "languages",
-    label: "languages",
-    order: 10,
-  },
-  {
-    id: "project-management",
-    label: "Project Management",
-    order: 8,
-  },
-  {
-    id: "modelling",
-    label: "Modelling",
-    order: 7,
-  },
-  {
-    id: "ide",
-    label: "IDE",
-    order: 9,
-  },
-  {
-    id: "sports",
-    label: "Sports",
-    order: 4,
-  },
-  {
-    id: "source-control",
-    label: "Source Control",
-    order: 4,
-  },
-  {
-    id: "operating-systems",
-    label: "Operating Systems",
-    order: 6,
-  },
-  {
-    id: "back-end-development",
-    label: "Back-end development",
-    order: 2,
-  },
-  {
-    id: "travel",
-    label: "Travel",
-    order: 3,
-  },
-  {
-    id: "server-administration",
-    label: "Server administration",
-    order: 3,
-  },
-  {
-    id: "front-end-development",
-    label: "Front-end development",
-    order: 1,
-  },
+  { id: "database", label: "Database", order: 5 },
+  { id: "miscellaneous", label: "Miscellaneous", order: 11 },
+  { id: "languages", label: "languages", order: 10 },
+  { id: "project-management", label: "Project Management", order: 8 },
+  { id: "modelling", label: "Modelling", order: 7 },
+  { id: "ide", label: "IDE", order: 9 },
+  { id: "sports", label: "Sports", order: 4 },
+  { id: "source-control", label: "Source Control", order: 4 },
+  { id: "operating-systems", label: "Operating Systems", order: 6 },
+  { id: "back-end-development", label: "Back-end development", order: 2 },
+  { id: "travel", label: "Travel", order: 3 },
+  { id: "server-administration", label: "Server administration", order: 3 },
+  { id: "front-end-development", label: "Front-end development", order: 1 },
 ];
--- test/fixtures/interest.ts
+++ test/fixtures/interest.ts
@@ -3,107 +3,43 @@ import { FirestoreInterest } from "../../src/types/interest";
 export const interestFixture = (): (Omit<FirestoreInterest, "category"> & {
   category: string;
 })[] => [
-  {
-    category: "travel",
-    name: "United States",
-    order: 8,
-  },
+  { category: "travel", name: "United States", order: 8 },
   {
     category: "miscellaneous",
     description: "SF, Fantasy, Manga/Comics",
     name: "Reading",
     order: 15,
   },
-  {
-    category: "sports",
-    name: "Trekking",
-    order: 12,
-  },
-  {
-    category: "travel",
-    name: "Ecuador",
-    order: 2,
-  },
+  { category: "sports", name: "Trekking", order: 12 },
+  { category: "travel", name: "Ecuador", order: 2 },
   {
     category: "sports",
     description: "advanced PADI diver",
     name: "Scuba Diving",
     order: 10,
   },
-  {
-    category: "travel",
-    name: "Myanmar (Burma)",
-    order: 4,
-  },
-  {
-    category: "miscellaneous",
-    name: "Photography",
-    order: 18,
-  },
-  {
-    category: "miscellaneous",
-    name: "Cooking",
-    order: 19,
-  },
-  {
-    category: "travel",
-    name: "Thailand",
-    order: 3,
-  },
-  {
-    category: "travel",
-    name: "Brazil",
-    order: 5,
-  },
+  { category: "travel", name: "Myanmar (Burma)", order: 4 },
+  { category: "miscellaneous", name: "Photography", order: 18 },
+  { category: "miscellaneous", name: "Cooking", order: 19 },
+  { category: "travel", name: "Thailand", order: 3 },
+  { category: "travel", name: "Brazil", order: 5 },
   {
     category: "travel",
     name: "6 months travel around Southeast Asia",
     order: 0,
   },
-  {
-    category: "sports",
-    name: "Tennis",
-    order: 14,
-  },
-  {
-    category: "travel",
-    name: "Spain",
-    order: 6,
-  },
-  {
-    category: "miscellaneous",
-    name: "Animation",
-    order: 17,
-  },
-  {
-    category: "travel",
-    name: "And many more...",
-    order: 9,
-  },
+  { category: "sports", name: "Tennis", order: 14 },
+  { category: "travel", name: "Spain", order: 6 },
+  { category: "miscellaneous", name: "Animation", order: 17 },
+  { category: "travel", name: "And many more...", order: 9 },
   {
     category: "miscellaneous",
     description: "Favorite directors: Stanley Kubrick's & Terry Gilliam's",
     name: "Cinema",
     order: 16,
   },
-  {
-    category: "sports",
-    name: "Mountain & road bicycling",
-    order: 11,
-  },
-  {
-    category: "sports",
-    name: "Roller skating",
-    order: 13,
-  },
-  {
-    category: "travel",
-    name: "Iceland",
-    order: 1,
-  },
-  {
-    category: "travel",
-    name: "China",
-    order: 7,
-  },
+  { category: "sports", name: "Mountain & road bicycling", order: 11 },
+  { category: "sports", name: "Roller skating", order: 13 },
+  { category: "travel", name: "Iceland", order: 1 },
+  { category: "travel", name: "China", order: 7 },
 ];
