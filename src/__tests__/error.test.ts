ea2b5657e4801b506a0e1ae2c08550fa5c6e2a3c
frank.gairal <frank@gairal.com>
2022-01-12 22:55:39 +0100

    fix: attempt at adding error tests

---
+++ src/__tests__/error.test.ts
@@ -0,0 +1,24 @@
+import * as request from "supertest";
+import { mockCollection } from "firestore-jest-mock/mocks/firestore";
+
+import { mockDb } from "../../test/fixtures";
+import { app } from "../app";
+
+mockDb();
+
+const subject = async (path: string) => request(app.callback()).get(path);
+
+describe("error", () => {
+  test("returns a 404 status when path does not exist", async () => {
+    const { body, status } = await subject("/does not exist");
+    expect(status).toBe(404);
+    expect(body).toStrictEqual({});
+  });
+
+  test.skip("returns a 500 on error", async () => {
+    const { body, status } = await subject("/educations");
+    expect(status).toBe(200);
+    expect(body).toHaveLength(3);
+    expect(mockCollection).toHaveBeenCalledTimes(1);
+  });
+});
