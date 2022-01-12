a848a4715e2263739926731c02afc97ead6d497f
frank.gairal <frank@gairal.com>
2022-01-12 20:19:18 +0100

    fix: fixture update

--- test/fixtures/skill.ts
+++ test/fixtures/skill.ts
@@ -2,667 +2,369 @@ import { FirestoreSkill } from "../../src/types/skill";
 
 export const skillFixture = (): FirestoreSkill[] => [
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     description: "",
-
     level: 100,
     name: "Functional CSS",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     description: "",
-
     level: 80,
     name: "ReactJS/Redux",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     description: "",
-
     level: 100,
     name: "BEM",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     description: "",
-
     level: 100,
     name: "Webpack",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     description: "",
-
     level: 70,
     name: "VueJS",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     description: "",
-
     level: 20,
     name: "GoLang",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5631986051842048"],
-    },
-
+    category: { kind: "category", path: ["category", "5631986051842048"] },
     level: 75,
     name: "SQL",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 50,
     name: "C#/Windows Phone",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5639445604728832"],
-    },
+    category: { kind: "category", path: ["category", "5639445604728832"] },
     description: "native language",
-
     level: 100,
     name: "French",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 25,
     name: "Java/Android API",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 50,
     name: "Python",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5644406560391168"],
-    },
+    category: { kind: "category", path: ["category", "5644406560391168"] },
     description:
       "Post-conception cost quantification, Optimisation of cost and time",
-
     name: "Planning & cost",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5692462144159744"],
-    },
-
+    category: { kind: "category", path: ["category", "5692462144159744"] },
     level: 80,
     name: "CentOS",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5672749318012928"],
-    },
-
+    category: { kind: "category", path: ["category", "5672749318012928"] },
     level: 80,
     name: "GIT, Gitlab",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5631986051842048"],
-    },
-
+    category: { kind: "category", path: ["category", "5631986051842048"] },
     level: 75,
     name: "Oracle",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 40,
     name: "Spring Framework",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5672749318012928"],
-    },
-
+    category: { kind: "category", path: ["category", "5672749318012928"] },
     level: 80,
     name: "SVN",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 90,
     name: "Grunt, Gulp",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5639445604728832"],
-    },
+    category: { kind: "category", path: ["category", "5639445604728832"] },
     description: "fluent - TOEIC: 820pts",
-
     level: 75,
     name: "English",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5634472569470976"],
-    },
-
+    category: { kind: "category", path: ["category", "5634472569470976"] },
     name: "Electronics",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5654313976201216"],
-    },
-
+    category: { kind: "category", path: ["category", "5654313976201216"] },
     name: "MERISE2",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5644406560391168"],
-    },
-
+    category: { kind: "category", path: ["category", "5644406560391168"] },
     name: "Technical specifications",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 80,
     name: "Foundation",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5634472569470976"],
-    },
-
+    category: { kind: "category", path: ["category", "5634472569470976"] },
     name: "Economics",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 75,
     name: "Bootstrap 3 & 4",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 75,
     name: "XSLT",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 50,
     name: "C/GTK+",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 75,
     name: "Visual Basic",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5634472569470976"],
-    },
-
+    category: { kind: "category", path: ["category", "5634472569470976"] },
     name: "Quality control",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     description: "SFR mobile Meta Language",
-
     level: 75,
     name: "PML",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 75,
     name: "VBA Excel & Access",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5644406560391168"],
-    },
+    category: { kind: "category", path: ["category", "5644406560391168"] },
     description: "Brainstorming, storyboard, managing graphic design, ...",
-
     name: "Conception",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5654313976201216"],
-    },
-
+    category: { kind: "category", path: ["category", "5654313976201216"] },
     name: "UML",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5634472569470976"],
-    },
-
+    category: { kind: "category", path: ["category", "5634472569470976"] },
     name: "Probabilities",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5644406560391168"],
-    },
+    category: { kind: "category", path: ["category", "5644406560391168"] },
     description: "Technical expertise, comercial proposal, quotation of cost",
-
     name: "Pre-sales",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     description: "Orange mobile Meta Language",
-
     level: 75,
     name: "OML",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 75,
     name: ".NET/C#/WPF",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 100,
     name: "sass, less",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5692462144159744"],
-    },
-
+    category: { kind: "category", path: ["category", "5692462144159744"] },
     level: 100,
     name: "Windows xp, Vista, 7, 8",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5631986051842048"],
-    },
-
+    category: { kind: "category", path: ["category", "5631986051842048"] },
     level: 100,
     name: "MySQL",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 75,
     name: "PHP",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 50,
     name: "Django",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5730082031140864"],
-    },
-
+    category: { kind: "category", path: ["category", "5730082031140864"] },
     level: 50,
     name: "Apache Tomcat",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5659313586569216"],
-    },
-
+    category: { kind: "category", path: ["category", "5659313586569216"] },
     name: "Dev-C++",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 90,
     name: "Material Design",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 100,
     name: "HTML 5",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5659313586569216"],
-    },
-
+    category: { kind: "category", path: ["category", "5659313586569216"] },
     name: "Eclipse",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5659313586569216"],
-    },
-
+    category: { kind: "category", path: ["category", "5659313586569216"] },
     name: "Netbeans",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 50,
     name: "Java/J2ee",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5659313586569216"],
-    },
-
+    category: { kind: "category", path: ["category", "5659313586569216"] },
     name: "Expression Blend",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5644406560391168"],
-    },
-
+    category: { kind: "category", path: ["category", "5644406560391168"] },
     name: "SCRUM Management",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5730082031140864"],
-    },
-
+    category: { kind: "category", path: ["category", "5730082031140864"] },
     level: 50,
     name: "Shell Script",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5644406560391168"],
-    },
-
+    category: { kind: "category", path: ["category", "5644406560391168"] },
     name: "MS project",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5730082031140864"],
-    },
-
+    category: { kind: "category", path: ["category", "5730082031140864"] },
     level: 70,
     name: "Apache httpd",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5634472569470976"],
-    },
-
+    category: { kind: "category", path: ["category", "5634472569470976"] },
     name: "Employment law",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5692462144159744"],
-    },
-
+    category: { kind: "category", path: ["category", "5692462144159744"] },
     level: 100,
     name: "Ubuntu / Debian",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5659313586569216"],
-    },
-
+    category: { kind: "category", path: ["category", "5659313586569216"] },
     name: "MS Visual Studio",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 100,
     name: "JQuery",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5631986051842048"],
-    },
-
+    category: { kind: "category", path: ["category", "5631986051842048"] },
     level: 75,
     name: "Access",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 25,
     name: "Flex",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 50,
     name: "C++",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5631986051842048"],
-    },
-
+    category: { kind: "category", path: ["category", "5631986051842048"] },
     level: 75,
     name: "MS SQLServer",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 75,
     name: "JavaScript",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
-
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     level: 100,
     name: "XML",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5639445604728832"],
-    },
+    category: { kind: "category", path: ["category", "5639445604728832"] },
     description: "Learning...",
-
     level: 10,
     name: "Vietnamese",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5692462144159744"],
-    },
-
+    category: { kind: "category", path: ["category", "5692462144159744"] },
     name: "Linux other distrbs.",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 100,
     name: "CSS3",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5654313976201216"],
-    },
-
+    category: { kind: "category", path: ["category", "5654313976201216"] },
     name: "MERISE",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5644406560391168"],
-    },
-
+    category: { kind: "category", path: ["category", "5644406560391168"] },
     name: "Team motivation",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5634472569470976"],
-    },
-
+    category: { kind: "category", path: ["category", "5634472569470976"] },
     name: "Marketing",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5659313586569216"],
-    },
-
+    category: { kind: "category", path: ["category", "5659313586569216"] },
     level: 90,
     name: "IntelliJ, PHPStorm",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5634472569470976"],
-    },
-
+    category: { kind: "category", path: ["category", "5634472569470976"] },
     name: "Nano-science",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5732568548769792"],
-    },
-
+    category: { kind: "category", path: ["category", "5732568548769792"] },
     level: 80,
     name: "Angular",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5692462144159744"],
-    },
+    category: { kind: "category", path: ["category", "5692462144159744"] },
     level: 100,
     name: "MacOS 10.x",
   },
   {
-    category: {
-      kind: "category",
-      path: ["category", "5700305828184064"],
-    },
+    category: { kind: "category", path: ["category", "5700305828184064"] },
     description: "",
     level: 85,
     name: "NodeJS",
