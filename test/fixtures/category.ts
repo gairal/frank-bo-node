16f3a28bd9f7397247589d6cfefc5a7da11221e6
frank.gairal <frank@gairal.com>
2022-01-12 21:52:53 +0100

    fix: tests

--- test/fixtures/category.ts
+++ test/fixtures/category.ts
@@ -3,67 +3,67 @@ import { FirestoreCategory } from "../../src/types/category";
 export const categoryFixture = (): FirestoreCategory[] => [
   {
     id: "database",
-    name: "Database",
+    label: "Database",
     order: 5,
   },
   {
     id: "miscellaneous",
-    name: "Miscellaneous",
+    label: "Miscellaneous",
     order: 11,
   },
   {
     id: "languages",
-    name: "languages",
+    label: "languages",
     order: 10,
   },
   {
     id: "project-management",
-    name: "Project Management",
+    label: "Project Management",
     order: 8,
   },
   {
     id: "modelling",
-    name: "Modelling",
+    label: "Modelling",
     order: 7,
   },
   {
     id: "ide",
-    name: "IDE",
+    label: "IDE",
     order: 9,
   },
   {
     id: "sports",
-    name: "Sports",
+    label: "Sports",
     order: 4,
   },
   {
     id: "source-control",
-    name: "Source Control",
+    label: "Source Control",
     order: 4,
   },
   {
     id: "operating-systems",
-    name: "Operating Systems",
+    label: "Operating Systems",
     order: 6,
   },
   {
     id: "back-end-development",
-    name: "Back-end development",
+    label: "Back-end development",
     order: 2,
   },
   {
     id: "travel",
-    name: "Travel",
+    label: "Travel",
     order: 3,
   },
   {
     id: "server-administration",
-    name: "Server administration",
+    label: "Server administration",
     order: 3,
   },
   {
     id: "front-end-development",
-    name: "Front-end development",
+    label: "Front-end development",
     order: 1,
   },
 ];
